37e481e4981747b91994b6bd71d4c9f48e2135a3
ViktorOsa <viktor.osadchyi1@gmail.com>
2020-12-01 16:25:53 +0100

    finish tasks from chapter 3 of lesson1

---
+++ lesson1/review/src/main/java/com/udacity/jwdnd/c1/review/MessageService.java
@@ -0,0 +1,33 @@
+package com.udacity.jwdnd.c1.review;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service
+public class MessageService {
+
+    private String message;
+
+    public MessageService(String message){
+        System.out.println("Bean MessageService is created (constructor with parameter)");
+        this.message = message;
+    }
+
+    public String uppercase(){
+        return this.message.toUpperCase();
+    }
+
+    public String lowercase(){
+        return this.message.toLowerCase();
+    }
+
+    @PostConstruct
+    public void postConstruct(){
+        System.out.println("Bean MessageService is created (post constructor)");
+    }
+
+
+
+
+}
